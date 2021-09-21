d261806146dc528379e28fdffbe2966278afb1b3
Diego Arthur <digo.arthur@hotmail.com>
2021-09-20 21:22:48 -0300

    add Cross-Env , Style, Css, Sass

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports ={
  presets:[
      '@babel/preset-env',
-     '@babel/preset-react'
+     ['@babel/preset-react',{
+
+        runtime:'automatic' // rodar a biblioteca react sem a importação
+     }]
  ]
 
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,45 @@
-const { dirname } = require('path')
-const path = require('path')
+// yarn webpack 
+
+//const { dirname } = require('path')
+const path = require('path') // utilizado para adaptação de caminho devido a posição da barra  "\" "/"
+const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const isDevelopment= process.env.NODE_ENV !== 'production' // utilizado para identificar se é ambiente de desenvolvimento 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
+    mode: isDevelopment ? 'development':'production', // modo de execução do webpack 
+    devtool: isDevelopment ? 'eval-source-map':'source-map'  , // Verificar Navegador. Utilizado para localizar o erro na linha correta, facilitar o debug
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),  // Arquivo inicial da aplicação
+    output: {// arquivo de saída
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    resolve: {
+    resolve: {// config. para leitura de arquivos
         extensions: ['.js', '.jsx'],
     },
-    module: {
+    devServer: {
+        static: path.resolve(__dirname, "public")
+       },
+    plugins:[
+new HtmlWebpackPlugin({
+    template:path.resolve(__dirname,'public','index.html') //config para <script src="../dist/bundle.js"></script> colocado no index.html
+})
+
+    ],
+    module: { // config. de como a aplicação irá se comportar diante de vários tipos de importação
         rules: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            use: 'babel-loader',
+            test: /\.jsx$/, // verifica se o arquivo é JS ou não para a importação. Indicando a extensão dele.
+            exclude: /node_modules/, // exclui todos os arquivos dentro desta pasta
+            use: 'babel-loader', // utilizar para visualizar / reconhecer
+
+        },
+
+        {
+            test: /\.scss$/, // 
+            exclude: /node_modules/, 
+            use: ['style-loader','css-loader','sass-loader'],
 
         }
+    
 
         ],
     }
